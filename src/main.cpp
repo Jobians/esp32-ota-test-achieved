3137bf8b360e510b1609077cc48f38e0f1dc7f58
Jobians <jobianstechie@gmail.com>
2025-10-24 13:05:52 +0100

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <WiFi.h>
 #include <ArduinoOTA.h>
 
-const char* ssid = "YourWiFiName";
-const char* password = "YourWiFiPassword";
+const char* ssid = "OPPO Reno12 5G";
+const char* password = "qiwa7765";
+
+const int ledPin = 33; // Onboard LED / flash pin
 
 void setup() {
   Serial.begin(115200);
+  
+  // Set LED pin as output
+  pinMode(ledPin, OUTPUT);
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -21,5 +27,10 @@ void setup() {
 
 void loop() {
   ArduinoOTA.handle();
-  // Your normal loop code here
+
+  // Blink the onboard LED
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
 }
